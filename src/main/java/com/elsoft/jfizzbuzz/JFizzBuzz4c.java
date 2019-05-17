27006bcc18771df3661353539ab91c29162c7749
Mark Elston <Mark.Elston@advantest.com>
2019-05-17 12:26:38 -0700

    Move composed function to class-scope.

--- src/main/java/com/elsoft/jfizzbuzz/JFizzBuzz4c.java
+++ src/main/java/com/elsoft/jfizzbuzz/JFizzBuzz4c.java
@@ -14,11 +14,10 @@ public class JFizzBuzz4c {
     static Function<Pair, Pair> do11 = UtilFns.genFB(11, "boom");
     static Function<Integer, Pair> init = UtilFns::createPair;  // (i) -> UtilFns.createPair(i);
     static Function<Pair, String> str   = Pair::toString;       // (p) -> p.toString();
+    static Function<Integer, String> composed =
+            init.andThen(do3).andThen(do5).andThen(do7).andThen(do11).andThen(str);
 
     public static void main(String[] args) {
-
-        Function<Integer, String> composed =
-                init.andThen(do3).andThen(do5).andThen(do7).andThen(do11).andThen(str);
 
         Stream.iterate(1, i -> i + 1)
                 .map(composed)
